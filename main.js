d040fd89a7233fc5be7911dccc24f37a63a9a8c0
estlop <lopezesteban8@gmail.com>
2022-06-24 19:40:01 +0100

    Get first 10 ids and cleanup

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const handleStories = () => {
   fetch("https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty")
     .then((showStoriesData) => showStoriesData.json())
-    .then((sData) => {
-      console.log(sData,  typeof sData[1]);
-      
-      const sortedData = [...sData].sort((a, b) => a > b ? -1 : 1)
-      console.log(sortedData);
-      return sortedData;
-    })
-    .then((sordedData) => {
-      console.log(sordedData);
-    });
+    .then((sData) => [...sData].sort((a, b) => a > b ? -1 : 1))
+    .then((sortedData) => sortedData.slice(0, 10))
+    .then(slicedData => console.log(slicedData));
 };
