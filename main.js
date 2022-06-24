c25fb3ed1a783d98e25013cf434b60df509c7281
estlop <lopezesteban8@gmail.com>
2022-06-24 19:36:23 +0100

    Sort data by id

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const handleStories = () => {
   fetch("https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty")
     .then((showStoriesData) => showStoriesData.json())
     .then((sData) => {
-      console.log(sData);
+      console.log(sData,  typeof sData[1]);
+      
+      const sortedData = [...sData].sort((a, b) => a > b ? -1 : 1)
+      console.log(sortedData);
+      return sortedData;
+    })
+    .then((sordedData) => {
+      console.log(sordedData);
     });
 };
