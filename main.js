61385e2aaef7d94383a762d08e81d19b12ff8408
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-06-24 20:31:37 +0100

    Get each data for each story

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const handleStories = () => {
-  fetch("https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty")
-    .then((showStoriesData) => showStoriesData.json())
-    .then((sData) => [...sData].sort((a, b) => a > b ? -1 : 1))
-    .then((sortedData) => sortedData.slice(0, 10))
-    .then(slicedData => console.log(slicedData));
+    const getStoriesData = async () => {
+        const showStoriesData = await fetch(
+            'https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty'
+        );
+        const sData = await showStoriesData.json();
+        const sortedData = [...sData].sort((a, b) => (a > b ? -1 : 1));
+        const slicedData = sortedData.slice(0, 10);
+        const showStories = await Promise.all([
+            ...slicedData.map((storyId) =>
+                fetch(
+                    `https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`
+                ).then((showStory) => showStory.json())
+            ),
+        ]);
+        return showStories;
+    };
+    getStoriesData().then((showStories) => {
+        console.log(showStories);
+    });
 };
